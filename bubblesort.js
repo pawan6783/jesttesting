43eb17c7f53e9db5eaf467fa8b7612a611e81397
Pawan Saroj <pawansaroj6783@gmail.com>
2020-09-02 10:26:55 +0530

    files modified

--- bubblesort.js
+++ bubblesort.js
@@ -4,22 +4,5 @@ swap = (arr, first , second) => {
     arr[second] = temp;
 }
 
-exports.bubblesort = function(list){
-    let len = list.length;
-    let i;
-    let j;
-    let stop;
+exports.bubblesort = 
 
-    for (i=0; i<len; i++)
-    {
-        for(j=0, stop=len-i; j<stop; j++)
-        {
-            if(list[j] >list[j+1]){
-                swap(list,j,j+1);
-            }
-        }
-    }
-    return list;
-}
-
-//console.log(bubble([5, 4, 8, 1, 9]));
--- bubblesort.test.js
+++ bubblesort.test.js
@@ -1,8 +1,39 @@
-const bubblesort = require("./bubblesort.js");
+const functions = require("./bubblesort");
+
+test("giving null value as input", () => {
+
+    const arr = null;
+
+    expect(functions.bubblesort(arr)).toBeNull();
+});
+
+
+test("giving an empty array as input", () => {
+
+    const arr = [];
+
+    expect(functions.bubblesort(arr)).toBe("Empty list");
+});
+
 
 test("sort all natural number", () => {
 
     const arr = [5, 4, 8, 1, 9];
 
-    expect(bubblesort.bubblesort(arr)).toEqual([1, 4, 5, 8, 9]);
+    expect(functions.bubblesort(arr)).toEqual([1, 4, 5, 8, 9]);
 });
+
+test("sort positive and negative numbers", () => {
+
+    const arr = [5, -4, 8, 1, -9];
+
+    expect(functions.bubblesort(arr)).toEqual([-9,-4, 1, 5, 8]);
+});
+
+test("sort floating point numbers and natural numbers", () => {
+
+    const arr = [5.2, 4, 8.8, 1.2, 9];
+
+    expect(functions.bubblesort(arr)).toEqual([1.2, 4, 5.2, 8.8, 9]);
+});
+
